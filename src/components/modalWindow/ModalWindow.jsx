d44836635b3da7f95fe5900996026b5c7e8af0ae
tskorohod <tskorohod@lmteam.by>
2022-11-15 23:07:59 +0300

    improved code readability

--- src/components/modalWindow/ModalWindow.jsx
+++ src/components/modalWindow/ModalWindow.jsx
@@ -2,39 +2,38 @@ import { useState } from "react";
 import "./_modalWindow.scss";
 
 const ModalWindow = (props) => {
-  const { active, setActive, choseId, author } = props;
-  const [inputValue, setinputValue] = useState(" ");
+	const { active, setActive, choseId, author } = props;
+	const [inputValue, setinputValue] = useState(" ");
 
-  const sendInfo = async () => {
-    let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify({ body: inputValue }),
-    });
-    if (response.status === 201) {
-      props.updataAfterDelete(choseId);
-      setinputValue(" ");
-    }
-  };
-  return (
-    <div
-      className={active ? "modal active" : "modal"}
-      onClick={() => setActive(false)}
-    >
-      <div className="modal__content" onClick={(e) => e.stopPropagation()}>
-        <div className="modal__author">{author}</div>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(event) => setinputValue(event.target.value)}
-        ></input>
-        <button className="modal__btn" onClick={() => sendInfo()} type="submit">
-          Отправить
-        </button>
-      </div>
-    </div>
-  );
+	const sendInfo = async () => {
+	let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+		method: "POST",
+		headers: {
+		"Content-Type": "application/json;charset=utf-8",
+	},
+	body: JSON.stringify({ body: inputValue }),
+	});
+	if (response.status === 201) {
+		props.updataAfterDelete(choseId);
+		setinputValue(" ");
+	}
+	};
+
+	return (
+	<div
+		className={active ? "modal active" : "modal"}
+		onClick={() => setActive(false)}
+	>
+		<div className="modal__content" onClick={(e) => e.stopPropagation()}>
+			<div className="modal__author">{author}</div>
+			<input
+				type="text"
+				value={inputValue}
+				onChange={(event) => setinputValue(event.target.value)}
+			></input>
+			<button className="modal__btn" onClick={() => sendInfo()} type="submit">Отправить</button>
+		</div>
+	</div>
+	);
 };
 export default ModalWindow;
