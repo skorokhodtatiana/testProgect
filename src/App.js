70eee187cf950075468446907cffcef78063b571
tskorohod <tskorohod@lmteam.by>
2022-11-16 17:22:38 +0300

    improved readbility code

--- src/App.js
+++ src/App.js
@@ -5,43 +5,44 @@ import { useState } from "react";
 import ModalWindow from "./components/modalWindow/ModalWindow";
 
 function App() {
-  const [modalActive, setmodalActive] = useState(false);
-  const [newData, setNewData] = useState();
-  const [chosenImgId, setchosenImgId] = useState(-1);
-  const [selectedAuthor, setSelectedAuthor] = useState();
+	const [modalActive, setmodalActive] = useState(false);
+	const [newData, setNewData] = useState();
+	const [chosenImgId, setchosenImgId] = useState(-1);
+	const [selectedAuthor, setSelectedAuthor] = useState();
 
-  const openModal = (val, id) => {
-    setmodalActive(true);
-    setNewData(val);
-    setchosenImgId(id);
-    let findAuthor = val.filter((el) => el.id === id);
-    setSelectedAuthor(findAuthor[0].author);
-  };
+	const openModal = (val, id) => {
+		setmodalActive(true);
+		setNewData(val);
+		setchosenImgId(id);
+		let findAuthor = val.filter((el) => el.id === id);
+		setSelectedAuthor(findAuthor[0].author);
+	};
 
-  const [isChangeArr, setisChangeArr] = useState(false);
-  const [isSelectImg, setisSelectImg] = useState(-1);
-  const updataAfterDelete = (choseId) => {
-    setisChangeArr(true);
-    setmodalActive(false);
-    setisSelectImg(choseId);
-  };
-  return (
-    <div className="App">
-      <ListImg
-        handleClick={(val, id) => openModal(val, id)}
-        isChangeArr={isChangeArr}
-        isSelectImg={isSelectImg}
-      ></ListImg>
-      <ModalWindow
-        author={selectedAuthor}
-        arrData={newData}
-        choseId={chosenImgId}
-        active={modalActive}
-        setActive={setmodalActive}
-        updataAfterDelete={updataAfterDelete}
-      ></ModalWindow>
-    </div>
-  );
+	const [isChangeArr, setisChangeArr] = useState(false);
+	const [isSelectImg, setisSelectImg] = useState(-1);
+	const updataAfterDelete = (choseId) => {
+		setisChangeArr(true);
+		setmodalActive(false);
+		setisSelectImg(choseId);
+	};
+
+	return (
+		<div className="App">
+			<ListImg
+				handleClick={(val, id) => openModal(val, id)}
+				isChangeArr={isChangeArr}
+				isSelectImg={isSelectImg}
+			></ListImg>
+			<ModalWindow
+				author={selectedAuthor}
+				arrData={newData}
+				choseId={chosenImgId}
+				active={modalActive}
+				setActive={setmodalActive}
+				updataAfterDelete={updataAfterDelete}
+			></ModalWindow>
+		</div>
+	);
 }
 
 export default App;
