956141c996d464733730eab8f3d26022c1fb1341
tskorohod <tskorohod@lmteam.by>
2022-11-16 17:02:23 +0300

    improved readbility code

--- src/components/listImg/ListImg.jsx
+++ src/components/listImg/ListImg.jsx
@@ -2,66 +2,65 @@ import { useState, useEffect } from "react";
 import "./listImg.scss";
 
 const ListImg = (props) => {
-  const { isChangeArr, isSelectImg } = props;
-  const [error, seterror] = useState(null);
-  const [isLoaded, setisLoaded] = useState(false);
-  const [items, setitems] = useState([]);
+	const { isChangeArr, isSelectImg } = props;
+	const [error, seterror] = useState(null);
+	const [isLoaded, setisLoaded] = useState(false);
+	const [items, setitems] = useState([]);
 
-  useEffect(() => {
-    fetch("https://picsum.photos/v2/list?page=2&limit=100")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setisLoaded(true);
-          setitems(result);
-        },
-        (error) => {
-          setisLoaded(true);
-          seterror(error);
-        }
-      );
-  }, []);
-  const passFunction = (id) => {
-    props.handleClick(items, id);
-  };
+	useEffect(() => {
+		fetch("https://picsum.photos/v2/list?page=2&limit=100")
+		.then((res) => res.json())
+		.then(
+			(result) => {
+				setisLoaded(true);
+				setitems(result);
+			},
+			(error) => {
+			setisLoaded(true);
+			seterror(error);
+			}
+		);
+	}, []);
 
-  if (error) {
-    return <div>Ошибка: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Загрузка...</div>;
-  } else {
-    return (
-      <ul className="listWrapper">
-        {!isChangeArr
-          ? items.map((item) => (
-              <li key={item.id}>
-                <div className="imgWrapper">
-                  <img
-                    onClick={() => passFunction(item.id)}
-                    className="img"
-                    src={item.download_url}
-                    alt={item.name}
-                  />
-                </div>
-              </li>
-            ))
-          : items
-              .filter((el) => el.id !== isSelectImg)
-              .map((item) => (
-                <li key={item.id}>
-                  <div className="imgWrapper">
-                    <img
-                      onClick={() => passFunction(item.id)}
-                      className="img"
-                      src={item.download_url}
-                      alt={item.name}
-                    />
-                  </div>
-                </li>
-              ))}
-      </ul>
-    );
-  }
+	const passFunction = (id) => {
+		props.handleClick(items, id);
+	};
+
+	if (error) {
+		return <div>Ошибка: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <div>Загрузка...</div>;
+	} else {
+		return (
+			<ul className="listWrapper">
+			{!isChangeArr
+				? items.map((item) => (
+					<li key={item.id}>
+						<div className="imgWrapper">
+							<img
+								onClick={() => passFunction(item.id)}
+								className="img"
+								src={item.download_url}
+								alt={item.name}
+							/>
+						</div>
+					</li>
+				))
+				: items.filter((el) => el.id !== isSelectImg).map((item) => (
+					<li key={item.id}>
+						<div className="imgWrapper">
+							<img
+								onClick={() => passFunction(item.id)}
+								className="img"
+								src={item.download_url}
+								alt={item.name}
+							/>
+						</div>
+					</li>
+				))}
+			</ul>
+		);
+	}
 };
 
 export default ListImg;
--- src/components/modalWindow/ModalWindow.jsx
+++ src/components/modalWindow/ModalWindow.jsx
@@ -6,34 +6,32 @@ const ModalWindow = (props) => {
 	const [inputValue, setinputValue] = useState(" ");
 
 	const sendInfo = async () => {
-	let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-		method: "POST",
-		headers: {
-		"Content-Type": "application/json;charset=utf-8",
-	},
-	body: JSON.stringify({ body: inputValue }),
-	});
-	if (response.status === 201) {
-		props.updataAfterDelete(choseId);
-		setinputValue(" ");
-	}
+		let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+			method: "POST",
+			headers: {
+			"Content-Type": "application/json;charset=utf-8",
+			},
+			body: JSON.stringify({ body: inputValue }),
+		});
+
+		if (response.status === 201) {
+			props.updataAfterDelete(choseId);
+			setinputValue(" ");
+		}
 	};
 
 	return (
-	<div
-		className={active ? "modal active" : "modal"}
-		onClick={() => setActive(false)}
-	>
-		<div className="modal__content" onClick={(e) => e.stopPropagation()}>
-			<div className="modal__author">{author}</div>
-			<input
-				type="text"
-				value={inputValue}
-				onChange={(event) => setinputValue(event.target.value)}
-			></input>
-			<button className="modal__btn" onClick={() => sendInfo()} type="submit">Отправить</button>
+		<div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+			<div className="modal__content" onClick={(e) => e.stopPropagation()}>
+				<div className="modal__author">{author}</div>
+				<input
+					type="text"
+					value={inputValue}
+					onChange={(event) => setinputValue(event.target.value)}
+				></input>
+				<button className="modal__btn" onClick={() => sendInfo()} type="submit">Отправить</button>
+			</div>
 		</div>
-	</div>
 	);
 };
 export default ModalWindow;
